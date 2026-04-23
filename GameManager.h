b16f3b4bbd1566088a9cb016812beb66b0c235aa
zhuangziting25-alt <zhuangziting25@gmail.com>
2026-04-23 10:51:31 +0800

    Add GameManager class with basic structure

--- GameManager.h
+++ GameManager.h
@@ -1,1 +1,18 @@
-456
+#ifndef GAMEMANAGER_H
+#define GAMEMANAGER_H
+
+#include "Board.h"
+
+class GameManager {
+private:
+    Board board;
+    Stone currentTurn;
+    bool isGameOver;
+
+public:
+    GameManager();
+    void start();
+    void switchTurn();
+};
+
+#endif
