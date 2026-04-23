138c3599cb5e358682db2c9644228d57eb9078ef
zhuangziting25-alt <zhuangziting25@gmail.com>
2026-04-23 10:50:32 +0800

    Define Board class for game functionality
    Added Board class definition with methods for gameplay.

--- Board.h
+++ Board.h
@@ -1,1 +1,21 @@
-// 棋盤類別
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <vector>
+
+enum class Stone { None, Black, White };
+
+class Board {
+private:
+    int size;
+    std::vector<std::vector<Stone>> grid;
+
+public:
+    Board(int s = 19);
+    void display();
+    bool placeStone(int x, int y, Stone color);
+    Stone getStone(int x, int y);
+    int getSize();
+};
+
+#endif
