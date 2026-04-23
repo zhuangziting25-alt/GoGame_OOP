c0eeb9ef4696c093b6993f9c062411ef3621a154
zhuangziting25-alt <zhuangziting25@gmail.com>
2026-04-23 10:51:55 +0800

    Implement GameManager class with game logic

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,1 +1,25 @@
-789
+#include "GameManager.h"
+#include <iostream>
+
+GameManager::GameManager() : board(19), currentTurn(Stone::Black), isGameOver(false) {}
+
+void GameManager::switchTurn() {
+    currentTurn = (currentTurn == Stone::Black) ? Stone::White : Stone::Black;
+}
+
+void GameManager::start() {
+    int x, y;
+    while (!isGameOver) {
+        board.display();
+        std::cout << (currentTurn == Stone::Black ? "黑棋" : "白棋") << "下子 (輸入 x y, 或輸入 -1 -1 結束): ";
+        std::cin >> x >> y;
+
+        if (x == -1) break;
+
+        if (board.placeStone(x, y, currentTurn)) {
+            switchTurn();
+        } else {
+            std::cout << "無效的位置，請重試！" << std::endl;
+        }
+    }
+}
